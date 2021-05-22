94d1571208ff3ef57c33cb236306aadec042e5e8
David Valderrama <dvalderrama08@gmail.com>
2021-05-22 16:02:12 -0700

    Added intern getSchool code and test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -2,9 +2,15 @@ const Intern = require("../lib/Intern");
 
 describe("Intern", () => {
     let intern = new Intern("2", "Intern Test", "intern@test.com", "UC Test")
-    it("getRole returns Intern", () => {
+    it("getRole should return Intern", () => {
         const expected = "Intern";
         const actual = intern.getRole();
         expect(actual).toEqual(expected);
     });
+    
+    it("getSchool should return UC Test", () => {
+        const expected = "UC Test";
+        const actual = intern.getSchool();
+        expect(actual).toEqual(expected);
+    })
 });
